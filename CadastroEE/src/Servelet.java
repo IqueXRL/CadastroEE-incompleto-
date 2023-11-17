c840cbb30c2a1c7995b6c2ea0cae44a626444e9c
Caique Thomaz <126598893+IqueXRL@users.noreply.github.com>
2023-11-16 23:12:25 -0300

    Add files via upload

---
+++ CadastroEE/src/Servelet.java
@@ -0,0 +1,19 @@
+package meu.pacote;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/meuServlet")
+public class MeuServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        response.getWriter().println("<h1>Olá, este é um exemplo de servlet Jakarta EE!</h1>");
+    }
+}
---
+++ CadastroEE/src/main/resources/MovimentoFacade.java
@@ -0,0 +1,11 @@
+package cadastroee.controller;
+
+import cadastroee.model.Movimento;
+import jakarta.ejb.Stateless;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
+import java.util.List;
+
+
+
